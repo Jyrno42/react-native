7efd4f4ac86056994c3a1602c0b5b0f79b46fc35
Jyrno Ader <jyrno42@gmail.com>
2018-08-30 23:54:58 +0300

    Add overlay color tests

---
+++ ReactAndroid/src/androidTest/java/com/facebook/react/tests/ImageOverlayColorTestApp.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.tests;
+
+import com.facebook.react.testing.ReactAppInstrumentationTestCase;
+
+/**
+ * Simple test case for passing overlayColor prop to the Image component
+ */
+public class ImageOverlayColorTestCase extends ReactAppInstrumentationTestCase {
+
+  @Override
+  protected String getReactApplicationKeyUnderTest() {
+    return "ImageOverlayColorTestApp";
+  }
+
+  public void testOverlayColorDoesNotCrash() {
+    ReactImageManager image = getViewByTestId("image");
+    assertNotNull(image);
+  }
+}
