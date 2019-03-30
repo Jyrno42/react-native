aedfcb6560f7020246bb037df21b62b86b851ee2
Jyrno Ader <jyrno42@gmail.com>
2019-03-30 21:16:33 +0200

    Make Touchables strict mode compatible
    Convert all Touchables to be class based and remove UNSAFE props. Also renamed
    Touchable.Mixin.withoutDefaultFocusAndBlur to Touchable.MixinWithoutDefaultFocusAndBlur
    to improve flow automatic typings.
    
    Note: TouchableNativeFeedback uses ReactNative.findNodeHandle which triggers a
    warning in strict mode during a tap. I could not figure out how to remove the need for
    that call.
    
    Related to https://github.com/facebook/react-native/issues/22186

--- RNTester/js/StrictModeExample.js
+++ RNTester/js/StrictModeExample.js
@@ -13,23 +13,73 @@
 const React = require('react');
 const {StrictMode} = React;
 const ReactNative = require('react-native');
-const {ScrollView, Text} = ReactNative;
+const {
+  ScrollView,
+  Text,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} = ReactNative;
+const TouchableBounce = require('TouchableBounce');
 
 type Props = $ReadOnly<{||}>;
 type State = {|result: string|};
 
-const componentsToTest = [ScrollView];
+const componentsToTest = [
+  [ScrollView, {}],
+  [
+    TouchableBounce,
+    {
+      onPress: () => console.warn('[press]'),
+    },
+  ],
+  [
+    TouchableHighlight,
+    {
+      onPress: () => console.warn('[press]'),
+    },
+  ],
+  [
+    // Caveat: Contains ReactNative.findNodeHandle which is not allowed in strict mode
+    TouchableNativeFeedback,
+    {
+      onPress: () => console.warn('[press]'),
+      background: TouchableNativeFeedback.Ripple('rgba(0, 0, 255, 0.4)', true),
+      children: (
+        <View style={{height: 100, backgroundColor: 'red'}}>
+          <Text style={{margin: 30}}>I am a TouchableNativeFeedback</Text>
+        </View>
+      ),
+    },
+  ],
+  [
+    TouchableOpacity,
+    {
+      onPress: () => console.warn('[press]'),
+    },
+  ],
+  [
+    TouchableWithoutFeedback,
+    {
+      onPress: () => console.warn('[press]'),
+    },
+  ],
+];
 
 class StrictModeExample extends React.Component<Props, State> {
   render() {
     return (
-      <StrictMode>
-        {componentsToTest.map(Component => (
-          <Component key={Component.displayName}>
-            <Text>{Component.displayName}</Text>
-          </Component>
-        ))}
-      </StrictMode>
+      <View>
+        <StrictMode>
+          {componentsToTest.map(([Component, props]) => (
+            <Component key={Component.displayName} {...props}>
+              {props.children || <Text>I am a {Component.displayName}</Text>}
+            </Component>
+          ))}
+        </StrictMode>
+      </View>
     );
   }
 }
